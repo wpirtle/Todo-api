333065a81578f8650c331aa4a75132686a819e15
Wayne Pirtle <whpirtle@gmail.com>
2015-12-31 13:35:20 -0600

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+// var person = {
+// 	name: 'Wayne',
+// 	age: 72
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.name = 'Andrew';
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [15, 88];
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	//gradesArr = [12, 33, 99];
+	debugger;
+}
+
+
+addGrades(grades);
+console.log(grades);
+
